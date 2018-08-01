f25566cfb27439cb365a48f175764f66521d11da
deker <dimon.bond.94@gmail.com>
2018-08-01 04:40:16 -0700

    Check status

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -4,7 +4,20 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Str;
+
 class Category extends Model
 {
-    //
+    //Mass assigned
+    protected $fillable = ['title', 'slug', 'published', 'parent_id', 'created_by', 'modified_by'];
+
+    //Mutators
+    public function setSlugAttribute($value){
+        $this->attributes['slug'] = Str::slug(mb_substr($this->title, 0, 40) . "-" . \Carbon\Carbon::now()->format('dmyHi'), '-');
+    }
+
+    //get children category
+    public function children(){
+        return $this->hasMany(self::class, 'parent_id');
+    }
 }
